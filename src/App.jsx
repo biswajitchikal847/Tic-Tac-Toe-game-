701245ff10ef13c65e1ef421b21423c66099b2d6
Biswajit Biswal <biswajitchikal847@gmail.com>
2022-11-02 12:07:25 +0530

    completed the game

---
+++ src/App.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap'
+import './App.css';
+import { useState } from 'react';
+
+import Icon from "./Components/Icon"
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+
+  const [isCross, setIscross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIscross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9)
+  }
+
+  const checkIsWinner = () => {
+    if(itemArray[0] === itemArray[1] && 
+      itemArray[0] === itemArray[2]  &&
+      itemArray[0] !== "empty"
+      ){
+          setWinMessage(`${itemArray[0]} wins `)
+      }else if(
+        itemArray[3] === itemArray[4] && 
+        itemArray[4] === itemArray[5]  &&
+        itemArray[3] !== "empty"
+        
+      ){
+            setWinMessage(`${itemArray[3]} wins`)
+      }else if(
+        itemArray[6] === itemArray[7] && 
+        itemArray[7] === itemArray[8]  &&
+        itemArray[6] !== "empty"
+      ){
+        setWinMessage(`${itemArray[6]} wins`)
+      }else if(
+        itemArray[0] === itemArray[3] && 
+        itemArray[3] === itemArray[6]  &&
+        itemArray[0] !== "empty"
+      ){
+        setWinMessage(`${itemArray[0]} wins`)
+      }else if(
+        itemArray[1] === itemArray[4] && 
+        itemArray[4] === itemArray[7]  &&
+        itemArray[1] !== "empty"
+      ){
+        setWinMessage(`${itemArray[1]} wins`)
+      }else if(
+        itemArray[2] === itemArray[5] && 
+        itemArray[5] === itemArray[8]  &&
+        itemArray[2] !== "empty"
+      ){
+        setWinMessage(`${itemArray[2]} wins`)
+      }else if(
+        itemArray[0] === itemArray[4] && 
+        itemArray[4] === itemArray[8]  &&
+        itemArray[0] !== "empty"
+      ){
+        setWinMessage(`${itemArray[0]} wins`)
+      }else if(
+        itemArray[2] === itemArray[4] && 
+        itemArray[4] === itemArray[6]  &&
+        itemArray[2] !== "empty"
+      ){
+        setWinMessage(`${itemArray[2]} wins`)
+      }
+  };
+
+  const changeItem = itemNumber => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" })
+    }
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "circle"
+      setIscross(!isCross)
+    } else {
+      return toast("already filled", { type: "error" })
+    }
+
+    checkIsWinner();
+  }
+
+
+  return (
+    <Container className="p-5">
+      <ToastContainer position='bottom-center' />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ?
+            (
+              <div className='mb-2 mt-2'>
+                <h1 className="text-success text-upparcase text-center">
+                  {winMessage}
+                </h1>
+                <Button color='success'
+                  block
+                  onClick={reloadGame}
+                >
+                  Reload the game
+                </Button>
+              </div>
+            ) : (
+              <h1 className="text-center text-warning">
+                {isCross ? "Cross" : "Circle"} turns
+              </h1>
+            )
+          }
+          <div className='grid'>
+            {itemArray.map((item, index) => {
+
+              return <Card color='warning' onClick={ () =>  changeItem(index) }>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            })}
+
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/Icon.jsx
@@ -0,0 +1,18 @@
+import { FaTimes, FaPen, FaRegCircle } from 'react-icons/fa'
+
+
+
+ const Icon = ({ name }) => {
+
+    switch (name) {
+        case 'circle':
+            return <FaRegCircle className='icons' />
+        case 'cross':
+            return <FaTimes className='icons' />
+        default:
+            return <FaPen className='icons' />
+    }
+
+}
+
+export default Icon;
